a88fd7fc4bcafe359a6e1b34dfe2d35ee5c0779a
Yuchan <32204149@dankook.ac.kr>
2025-05-17 23:59:22 +0900

    test : keybert model + fix redundancy problem

--- nodes.py
+++ nodes.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
-from transformers import pipeline # 키워드 추출 패키지
-
+#from transformers import pipeline # 키워드 추출 패키지
+from keybert import KeyBERT
 import torch
 
 import os
-import sys
+import sys  
 import json
 import hashlib
 import traceback
@@ -48,6 +48,7 @@ def interrupt_processing(value=True):
 
 import requests
 from langdetect import detect
+
 # 번역기 api
 class DeepLTranslator:
     def __init__(self, api_key: str):
@@ -83,7 +84,6 @@ MAX_RESOLUTION=16384
 class CLIPTextEncode(ComfyNodeABC):
     @classmethod
     def INPUT_TYPES(s) -> InputTypeDict:
-
         return {
             "required": {
                 "text": (IO.STRING, {"multiline": True, "dynamicPrompts": True, "tooltip": "The text to be encoded."}),
@@ -101,6 +101,35 @@ class CLIPTextEncode(ComfyNodeABC):
         self.translator = DeepLTranslator("e7a84eba-0af1-4b37-aa36-f58c7c556ae9:fx")  # deepl 번역기 api key
 
     def encode(self, clip, text):
+        if clip is None:
+            raise RuntimeError("ERROR: clip input is invalid: None")
+
+        textEN = self.translator.translate_if_needed(text)
+
+        kw_model = KeyBERT(model='paraphrase-MiniLM-L6-v2')
+
+        #kw_model = KeyBERT()
+        keywords = kw_model.extract_keywords(
+            textEN,
+            keyphrase_ngram_range=(1, 4),
+            stop_words='english',
+            use_mmr=True,       #  중복 키워드 감소를 위한 MMR 사용
+            diversity=0.7,      #  다양성 확보를 위한 값 (0~1)
+            top_n=5             #  추출할 키워드 개수 제한
+        )
+
+        print("번역: ", textEN)
+        # Step 3: 프롬프트 출력
+        keyword_prompt = ", ".join([kw[0] for kw in keywords])
+        print("프롬프트 키워드: ", keyword_prompt)
+        tokens = clip.tokenize(keyword_prompt)
+
+        return (clip.encode_from_tokens_scheduled(tokens), )
+
+
+"""
+    아래는 원본 코드
+    def encode(self, clip, text):
 
         if clip is None:
             raise RuntimeError("ERROR: clip input is invalid: None")
@@ -108,10 +137,12 @@ class CLIPTextEncode(ComfyNodeABC):
         textEN = self.translator.translate_if_needed(text)
         nlp = pipeline("text2text-generation", model="ml6team/keyphrase-extraction-distilbert-inspec") # 텍스트 키워드 추출기
         textENKeyword = nlp(textEN)[0]['generated_text']
+
+        print(textENKeyword) # 키워드 추출기 체크
+
         tokens = clip.tokenize(textENKeyword)
         return (clip.encode_from_tokens_scheduled(tokens), )
-
-
+"""
 
 class ConditioningCombine:
     @classmethod
